b355e540195c4277c0d8a887501a87e45b6819a9
jimit-nayee <nayeejimit2002@gmail.com>
2023-08-11 15:14:33 +0530

    home landing page created

--- src/components/HomeImage/HomeImage.jsx
+++ src/components/HomeImage/HomeImage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./HomePage.module.css"
 
 const HomeImage = () => {
   return (
@@ -16,23 +15,50 @@ const HomeImage = () => {
           <img
             src="HomePage_Image.png"
             alt=""
-            style={{ height: "83vh", width: "100vw", opacity: 0.7 }}
+            style={{
+              height: "100vh",
+              width: "100vw",
+              opacity: 0.7,
+              transform: "rotateY(180deg)",
+            }}
           />
         </div>
 
         <div
           className="absolute text-white rounded-md p-3 "
-          style={{ top: "140px", left: "20px", width: "30vw" }}
+          style={{
+            top: "85px",
+            left: "20px",
+            height: "85vh",
+            width: "39vw",
+            backgroundColor: "#001C30",
+          }}
         >
-          <p className="text-3xl font-bold ">We Are The Solution</p>
-          <p className="text-sm mt-4">
+         <div style={{display:"flex",alignItems:"center",flexDirection:"column",justifyContent:"center",padding:"0 10px"}}> 
+         <p className="text-6xl font-bold" > <span style={{color:"orange"}}>We Are The </span> <span >Solution</span></p>
+         
+          <p className="text-lg mt-4 " style={{textAlign:"justify"}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
             fringilla, orci vel suscipit feugiat, turpis tellus porttitor enim,
             placerat molestie ligula nisl imperdiet leo. Nunc euismod dignissim
             sem, molestie pulvinar lectus venenatis vitae. Sed nisl orci,
             viverra in maximus
           </p>
+         </div>
         </div>
+        <div
+          style={{
+            width: "400px",
+            background: " rgb(0, 28, 48)",
+            position: "absolute",
+            top: "181px",
+            left: "244px",
+            /* z-index: 4, */
+            height: "400px",
+            transform: " rotate(45deg)",
+            zIndex: "-1",
+          }}
+        ></div>
       </div>
     </div>
   );
