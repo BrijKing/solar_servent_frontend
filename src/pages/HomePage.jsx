54acfe0232b49b8a40d5bbda879f60ca451abaa2
brijkig <brijeshvaishnav508@gmail.com>
2023-08-11 11:15:07 +0530

    create Home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+      <HomeImage />
+      <Features />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
