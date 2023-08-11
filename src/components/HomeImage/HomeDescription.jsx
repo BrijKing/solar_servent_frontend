668b5dc1da7c46174a83d4a315464810ec916bf1
jimit-nayee <nayeejimit2002@gmail.com>
2023-08-11 16:41:32 +0530

    saperate home image and description

---
+++ src/components/HomeImage/HomeDescription.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styles from "./HomePage.module.css"
+
+const HomeDescription = () => {
+  return (
+    <div>
+      <div
+        className={`absolute text-white rounded-md p-3 ${styles.infoDiv}`}
+        style={{
+          top: "85px",
+          left: "20px",
+          height: "85vh",
+          width: "39vw",
+          backgroundColor: "#001C30",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+            padding: "0 10px",
+          }}
+        >
+          <p className="text-6xl font-bold">
+            {" "}
+            <span style={{ color: "orange" }}>We Are The </span>{" "}
+            <span>Solution</span>
+          </p>
+
+          <p className="text-lg mt-4 " style={{ textAlign: "justify" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            fringilla, orci vel suscipit feugiat, turpis tellus porttitor enim,
+            placerat molestie ligula nisl imperdiet leo. Nunc euismod dignissim
+            sem, molestie pulvinar lectus venenatis vitae. Sed nisl orci,
+            viverra in maximus
+          </p>
+        </div>
+      </div>
+      <div
+        className={`${styles.infoDiv}`}
+        style={{
+          width: "400px",
+          background: " rgb(0, 28, 48)",
+          position: "absolute",
+          top: "181px",
+          left: "244px",
+          /* z-index: 4, */
+          height: "400px",
+          transform: " rotate(45deg)",
+          zIndex: "-1",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default HomeDescription;
--- src/components/HomeImage/HomeImage.jsx
+++ src/components/HomeImage/HomeImage.jsx
@@ -1,78 +1,29 @@
 import React from "react";
-import styles from "./HomePage.module.css"
+import HomeDescription from "./HomeDescription";
 
 const HomeImage = () => {
   return (
     <div>
-      <div>
-        <div
+      <div
+        style={{
+          background: "black",
+          position: "absolute",
+          top: 0,
+          zIndex: -1,
+        }}
+      >
+        <img
+          src="HomePage_Image.png"
+          alt=""
           style={{
-            background: "black",
-            position: "absolute",
-            top: 0,
-            zIndex: -1,
+            height: "100vh",
+            width: "100vw",
+            opacity: 0.7,
+            transform: "rotateY(180deg)",
           }}
-        >
-          <img
-            src="HomePage_Image.png"
-            alt=""
-            style={{
-              height: "100vh",
-              width: "100vw",
-              opacity: 0.7,
-              transform: "rotateY(180deg)",
-            }}
-          />
-        </div>
-
-        <div
-          className={`absolute text-white rounded-md p-3 ${styles.infoDiv}`}
-          style={{
-            top: "85px",
-            left: "20px",
-            height: "85vh",
-            width: "39vw",
-            backgroundColor: "#001C30",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "column",
-              justifyContent: "center",
-              padding: "0 10px",
-            }}
-          >
-            <p className="text-6xl font-bold">
-              {" "}
-              <span style={{ color: "orange" }}>We Are The </span>{" "}
-              <span>Solution</span>
-            </p>
-
-            <p className="text-lg mt-4 " style={{ textAlign: "justify" }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              fringilla, orci vel suscipit feugiat, turpis tellus porttitor
-              enim, placerat molestie ligula nisl imperdiet leo. Nunc euismod
-              dignissim sem, molestie pulvinar lectus venenatis vitae. Sed nisl
-              orci, viverra in maximus
-            </p>
-          </div>
-        </div>
-        <div  className={`${styles.infoDiv}`}
-          style={{
-            width: "400px",
-            background: " rgb(0, 28, 48)",
-            position: "absolute",
-            top: "181px",
-            left: "244px",
-            /* z-index: 4, */
-            height: "400px",
-            transform: " rotate(45deg)",
-            zIndex: "-1",
-          }}
-        ></div>
+        />
       </div>
+      <HomeDescription />
     </div>
   );
 };
