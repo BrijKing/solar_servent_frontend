c2a6189b659f8aee344b063c9b1d57d938bafb8e
brijkig <brijeshvaishnav508@gmail.com>
2023-08-17 18:22:26 +0530

    created auth provider

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Login from "./pages/Login";
 import { Route, Routes } from "react-router";
 import Register from "./pages/Register";
 import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevtools } from "react-query-devtools";
+import { AuthProvider } from "./contexts/AuthContext";
 
 const App = () => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthProvider>
       {/* <ReactQueryDevtools initialIsOpen={false} position="bottom-right" /> */}
     </QueryClientProvider>
   );
