adc5da6618e542f0d4445f0159d0281c8c5b30bf
jimit-nayee <nayeejimit2002@gmail.com>
2023-08-18 23:28:19 +0530

    button desgin

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const MenuItem = ({ name , path }) => {
+const MenuItem = ({ name, path }) => {
   return (
-    <div className="text-white">
-      <NavLink to={path}>{name}</NavLink>
+    <div className="w-full flex justify-center">
+      <NavLink to={path} className="flex justify-center w-full" >
+        <button className="w-full h-[60px] hover:bg-white hover:text-blue-900 hover:border-blue-900 text-white font-semibold py-2 px-4">
+          {name}
+        </button>
+      </NavLink>
     </div>
   );
 };
--- src/components/MenuItems/MenuItems.jsx
+++ src/components/MenuItems/MenuItems.jsx
@@ -17,11 +17,9 @@ const MenuItems = () => {
     },
   ];
   return (
-    <div className="absolute top-[200px] flex flex-col">
+    <div className="absolute top-[200px] w-full items-center flex flex-col">
       {links.map((link) => (
-        <>
-          <MenuItem name={link.name} path={link.path} />;
-        </>
+        <MenuItem name={link.name} path={link.path} />
       ))}
     </div>
   );
