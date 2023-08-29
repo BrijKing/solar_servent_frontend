7cd556c61f17c3fb9ad7152d42fa1a1db83f6ac4
brijkig <brijeshvaishnav508@gmail.com>
2023-08-29 14:29:28 +0530

    create modal for pdf

--- src/components/Review Customer/ReviewCustomer.jsx
+++ src/components/Review Customer/ReviewCustomer.jsx
@@ -13,13 +13,46 @@ import { AiFillEye } from "react-icons/ai";
 import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PdfViewer from "../Pdf-viewer/PdfViewer";
+import Modal from "../Modal/Modal";
 
 const ReviewCustomer = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const [reviewCustomer, setReviewCustomer] = useState([]);
   const [tableUpdated, setTableUpdated] = useState(false);
   const [pdfData, setPdfData] = useState(null);
 
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  useEffect(() => {
+    getPandingCustomerForApprovement().then((res) => {
+      let data = res.data.map((record) => {
+        return {
+          email: record.email,
+          signedBy: record.signBy,
+          actions: (
+            <div className={`${styles["action-btn"]}`}>
+              <button onClick={() => handleView(record.email)}>
+                <AiFillEye />
+              </button>
+              <button onClick={() => handleApprove(record.email)}>
+                <FontAwesomeIcon icon={faCheck} />
+              </button>
+              <button onClick={() => handleDisApprove(record.email)}>
+                <FontAwesomeIcon icon={faTimes} />
+              </button>
+            </div>
+          ),
+        };
+      });
+
+      setReviewCustomer(data);
+    });
+  }, [tableUpdated]);
+
   function handleView(email) {
+    openModal();
     getCustomerPdfById(email).then((res) => {
       if (res.status === 200) {
         setPdfData(res.data.pdfData);
@@ -52,38 +85,16 @@ const ReviewCustomer = () => {
     });
   }
 
-  useEffect(() => {
-    getPandingCustomerForApprovement().then((res) => {
-      let data = res.data.map((record) => {
-        return {
-          email: record.email,
-          signedBy: record.signBy,
-          actions: (
-            <div className={`${styles["action-btn"]}`}>
-              <button onClick={() => handleView(record.email)}>
-                <AiFillEye />
-              </button>
-              <button onClick={() => handleApprove(record.email)}>
-                <FontAwesomeIcon icon={faCheck} />
-              </button>
-              <button onClick={() => handleDisApprove(record.email)}>
-                <FontAwesomeIcon icon={faTimes} />
-              </button>
-            </div>
-          ),
-        };
-      });
-
-      setReviewCustomer(data);
-    });
-  }, [tableUpdated]);
   return (
     <div>
       <CustomTable
         fields={["Email", "Added By", "Actions"]}
         data={reviewCustomer}
       />
-      <PdfViewer pdfData={pdfData} />
+
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <PdfViewer pdfData={pdfData} />
+      </Modal>
     </div>
   );
 };
